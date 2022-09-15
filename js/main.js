7ae08bfd0d59cb3b044184894559a04ff3b03164
Jesus Benjamin Escacena <jesubes@gmail.com>
2022-09-15 06:46:24 -0300

    trabajo sobre el estilo base para despues incorporar JavaScript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+import data1 from '../data1.json' assert {type: 'json'};
+
+let dailyBtn = document.querySelector('#daily');
+let weklyBtn = document.querySelector('#wekly');
+let monthlyBtn = document.querySelector('#monthly');
+
+console.log(data1);
+
+dailyBtn.addEventListener('click', ()=>{3
+    console.log('se hizo clic')
+})
